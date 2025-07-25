55092524ceb17345d3d3f5b8e3abb796cf0fa2a8
RohitGusai <rohitgusai235016@gmail.com>
2025-07-25 06:20:03 +0530

    this is third changes

--- 01_index.js
+++ 01_index.js
@@ -1,3 +1,12 @@
 console.log("Hell Rohit")
 console.log("let's start coding")
 console.log("second try")
+
+const a = "hello"
+let b = "world"
+b = "everyone"
+
+console.table([a,b])
+/*
+never use var use let and const 
+*/
