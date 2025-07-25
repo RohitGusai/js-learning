612f8996c59d7b9101ce1386bfc9c8c4204c2f8e
RohitGusai <rohitgusai235016@gmail.com>
2025-07-25 07:02:28 +0530

    postfix prefix

---
+++ 03_prefix.js
@@ -0,0 +1,14 @@
+console.log(2+2+"2") // number add first then string concatenation
+console.log(2+"2"+2) // string concatenation first then number addition
+console.log("2"+2+2) // string concatenation first then number addition
+
+
+let a = 3
+let b = a++
+console.log(a) // 4
+console.log(b) // 3
+
+let x = 3
+let y = ++x
+console.log(x) // 4 
+console.log(y) // 4
