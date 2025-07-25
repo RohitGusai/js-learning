8ae0d99cf1f3059787111a63e0d7fb755a5da7ea
RohitGusai <rohitgusai235016@gmail.com>
2025-07-25 06:33:40 +0530

    this is datatype and conversion of varaible value

---
+++ 02_datatype.js
@@ -0,0 +1,23 @@
+/*
+string 
+number
+boolean 
+null  suppose a variable temperature and it is store 0 so you cannot show 0 because it is a temperature in that case you can use null
+
+undefined a variable is declared but not initialized
+symbol unique identifier
+bigint
+object 
+
+*/
+
+
+let a = "hello"
+let b = "33"
+let d = "33abc"
+console.log(typeof a) // string
+let c = Number(b)
+console.log(typeof c) // Number
+
+console.log(Number(typeof d)) // NaN not a number
+
