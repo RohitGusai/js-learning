1b618ff3ee173a2a95f11afc90dacbf26812cf18
RohitGusai <rohitgusai235016@gmail.com>
2025-07-27 22:22:58 +0530

    2 video completed

---
+++ 06_Date.js
@@ -0,0 +1,25 @@
+const myDate = new Date();
+console.log(myDate.toString())
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth()); 
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+
+
+let time = Date.now();
+console.log(typeof time)
+console.log(time);
+
+console.log(myDate.getHours());
+
+
+
+
+
+
+
---
+++ 07_array.js
@@ -0,0 +1,25 @@
+const newArray = [1, 2, 3, 4, 5,"rohit"]; 
+
+console.log(typeof newArray); // object
+
+console.log(newArray[5]);
+
+
+const a = newArray.join(); // joins convert the array into a string and is type from object to string
+console.log(a); // "1,2,3,4,5,rohit"
+console.log(typeof a); // string
+
+
+// the difference between slice and splice is that slice does not change the original array, while splice modifies it.
+
+const array1 = [1,2,3,4,5];
+console.log(array1);
+console.log(array1.slice(1,3));
+console.log("A : "+array1);
+
+const array2 = array1.splice(1,3);
+console.log(array2)
+console.log("B : "+array1);
+
+
+
